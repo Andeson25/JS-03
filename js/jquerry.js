cd575ea2a3e11ad5d81eb46bcb4db92e8a7f31b9
kek <oleh.petryk@gmail.com>
2017-12-11 18:47:03 +0200

    c

--- js/jquerry.js
+++ js/jquerry.js
@@ -1,1 +1,10 @@
-
+$(".buttons button").click(ev => {
+  $(".buttons button, .tabs div").removeClass("active");
+  $(ev.target).addClass("active");
+  const tab = ev.target.dataset.tab;
+  $(".tabs div").each((idx, el) => {
+    if (el.dataset.tab === tab) {
+      el.classList.add("active");
+    }
+  });
+});
