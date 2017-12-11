6776a5de29a364dd7ae9e18173dd5ffcbd8ed41c
kek <oleh.petryk@gmail.com>
2017-12-11 18:24:38 +0200

    c

--- js/index.js
+++ js/index.js
@@ -8,3 +8,17 @@
 //         target.hidden=true;
 //     }
 // },1)
+document.querySelectorAll(".buttons button").forEach(el => {
+  el.addEventListener("click", ev => {
+    document.querySelectorAll(".buttons button, .tabs div").forEach(el => {
+      el.classList.remove("active");
+    });
+    ev.target.classList.add("active");
+    const tab = ev.target.dataset.tab;
+    document.querySelectorAll(".tabs div").forEach(el => {
+      if (el.dataset.tab === tab) {
+        el.classList.add("active");
+      }
+    });
+  });
+});
