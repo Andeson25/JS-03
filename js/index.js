2b2f8a79c4086463f3c46ffd1cc9e339b28c9ff6
kek <oleh.petryk@gmail.com>
2017-12-11 17:56:00 +0200

    c

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// setInterval(()=> {
+//     target.innerHTML=(new Date()).toDateString()+' : '+(new Date()).toLocaleTimeString();
+// },10)
+// setInterval(()=>{
+//     if(target.hidden){
+//         target.hidden=false;
+//     }else{
+//         target.hidden=true;
+//     }
+// },1)
